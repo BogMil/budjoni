1e245983458176da51cf47001dfac0ee4e03fd46
BogMil <milanbogdanovic11@hotmail.com>
2020-09-21 22:18:08 +0200

    Add project files.

---
+++ Buđoni.DAL/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Buđoni.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Buđoni.DAL
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options){}
+
+        public DbSet<ModelObuce> ModeliObuce { get; set; }
+    }
+}
---
+++ Buđoni.DAL/Models/ModelObuce.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buđoni.Data
+{
+    public class ModelObuce
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string NazivModela{ get; set; }
+        public string Slika{ get; set; }
+    }
+}
